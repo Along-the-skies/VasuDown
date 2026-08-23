cc562cc4431b54eff4114bb1a7eb779e86474332
Vasudev <shajcheruvath@gmail.com>
2026-08-23 21:06:57 +0530

    Edit Metadata.py

--- app/services/metadata.py
+++ app/services/metadata.py
@@ -1,5 +1,10 @@
+from pathlib import Path
 from app.utils.urls import identify_platform
 import yt_dlp
+
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+COOKIES_FILE = PROJECT_ROOT / "cookies.txt"
 
 def get_metadata(url):
     platform = identify_platform(url)
@@ -7,26 +12,27 @@ def get_metadata(url):
     if platform is None:
         return None
 
-    if platform =="youtube":
-        ydl_opts =  {
-            "quiet":True,
-            "no_warnings":True,
-            "skip_download":True,
+    if platform == "youtube":
+        ydl_opts = {
+            "quiet": True,
+            "no_warnings": True,
+            "skip_download": True,
+            "cookiefile": str(COOKIES_FILE),
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url,download=False)
+            info = ydl.extract_info(url, download=False)
 
         return {
-                "url":url,
-                "platform":platform,
-                "title":info.get("title"),
-                "thumbnail":info.get("thumbnail"),
-            }
+            "url": url,
+            "platform": platform,
+            "title": info.get("title"),
+            "thumbnail": info.get("thumbnail"),
+        }
 
     return {
-        "url":url,
-        "platform":platform,
-        "title":None,
-        "thumbnail":None
+        "url": url,
+        "platform": platform,
+        "title": None,
+        "thumbnail": None,
     }
