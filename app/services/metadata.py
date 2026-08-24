8b44407c7f32b4a7eef61d0ba5b0e8a4d1a09ed4
Vasudev <shajcheruvath@gmail.com>
2026-08-24 12:57:27 +0530

    Restore Metadata

--- app/services/metadata.py
+++ app/services/metadata.py
@@ -18,6 +18,8 @@ def get_metadata(url):
             "no_warnings": True,
             "skip_download": True,
             "cookiefile": str(COOKIES_FILE),
+            "format": "bestvideo[vcodec^=avc1]+bestaudio[acodec^=mp4a]/best[ext=mp4]",
+            "merge_output_format": "mp4",
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
